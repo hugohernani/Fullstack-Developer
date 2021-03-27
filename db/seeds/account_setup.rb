c39f686bbd49442c42f8a6357013f22f63ded1c0
Hugo Hernani <hhernanni@gmail.com>
2021-03-27 03:32:04 -0300

    Add no_admin user seed

--- db/seeds/account_setup.rb
+++ db/seeds/account_setup.rb
@@ -1,3 +1,9 @@
-User.find_or_create_by(full_name: 'Fullstack Web Developer', email: 'admin@localhost.com', role: User.roles[:admin]) do |account|
+User.find_or_create_by(full_name: 'Admin Fullstack Web Developer', email: 'admin@localhost.com',
+                       role: User.roles[:admin]) do |account|
+  account.password = account.password_confirmation = ENV['DEV_PASSWORD'] || '12341234'
+end
+
+User.find_or_create_by(full_name: 'Fullstack Web Developer', email: 'user@localhost.com',
+                       role: User.roles[:no_admin]) do |account|
   account.password = account.password_confirmation = ENV['DEV_PASSWORD'] || '12341234'
 end
