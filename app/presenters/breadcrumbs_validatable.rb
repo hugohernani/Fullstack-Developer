3d1e17c15adbffc9a9bfd97a64ba09b03c6ff24f
Hugo Hernani <hhernanni@gmail.com>
2021-03-28 18:49:47 -0300

    Fix typo

--- app/presenters/breadcrumbs_validatable.rb
+++ app/presenters/breadcrumbs_validatable.rb
@@ -3,6 +3,6 @@ module BreadcrumbsValidatable
     required_attrs_presence = required_attrs.all? do |required_attr|
       breadcrumbs_options.key?(required_attr)
     end
-    required_attrs_presence || (raise StandardError, "Missing required paramemters: #{required_attrs}")
+    required_attrs_presence || (raise StandardError, "Missing required attributes: #{required_attrs}")
   end
 end
