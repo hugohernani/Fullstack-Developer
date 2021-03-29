0a8211a3924005a47b48b0ce2d844b4d66edacd1
Hugo Hernani <hhernanni@gmail.com>
2021-03-29 14:02:56 -0300

    Include admin and member traits into User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,17 @@
 FactoryBot.define do
   factory :user do
-    full_name { Faker::Name.full_name }
-    role { User.roles[:no_admin] }
+    full_name { Faker::Name.name }
+    role { User.roles[:member] }
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
+
+    trait :admin do
+      role { User.roles[:admin] }
+    end
+
+    trait :member do
+      role { User.roles[:member] }
+    end
   end
 end
