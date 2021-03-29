d6068f9b24a5e9da58e0e29ccc3e013df404080c
Hugo Hernani <hhernanni@gmail.com>
2021-03-29 03:45:44 -0300

    Add ApplicationCable connection with current_user warden setup

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_account
+      logger.add_tags 'ActionCable', current_user.email
+    end
+
+    protected
+
+    def find_verified_account
+      if (verified_account = env['warden'].user)
+        verified_account
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
